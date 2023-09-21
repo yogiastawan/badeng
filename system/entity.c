7c93e0fbdf5fa2b6c5dd9125aed53e18f33dd732
Yogi Astawan <yogi.astawan@gmail.com>
2023-09-22 01:05:42 +0800

    change impl new entity and add entity destroy

--- system/entity.c
+++ system/entity.c
@@ -1,8 +1,28 @@
 #include <system/entity.h>
+
 #include <stdlib.h>
 
-BeEntity be_entity_new()
+#include <utilities/checker.h>
+
+BeEntity *be_entity_new()
 {
-    BeEntity entt = {0, 0, DEFAULT_COMPONENT_CAPACITY, NULL};
+    BeEntity *entt = (BeEntity *)malloc(sizeof(BeEntity));
+    entt->id = 0;
+    u32 i = 0;
+    for (i = 0; i < NUMB_COMPONENT; i++)
+    {
+        entt->component_id[i] = 0;
+        entt->has_component[i] = false;
+    }
+
     return entt;
+}
+
+void be_entity_destroy(BeEntity *entt)
+{
+    if (NULL == entt)
+    {
+        return;
+    }
+    DESTROYER(entt);
 }
