3155b9050ee02fda3fb023b1dcafe4ee9e9002db
Yogi Astawan <yogi.astawan@gmail.com>
2023-09-22 01:02:20 +0800

    add DESTROYER and NULLER macro

--- include/utilities/checker.h
+++ include/utilities/checker.h
@@ -9,7 +9,6 @@ extern C
 #endif
 
 #ifdef GX_DEBUG
-
 #define VAR_NAME(X) #X
 #define NULL_CHECKER(X)                      \
     if (NULL == X)                           \
@@ -23,12 +22,20 @@ extern C
         LOGE("\"%s\" is NULL", VAR_NAME(X)); \
         return V;                            \
     }
-
 #else
 #define VAR_NAME(X)
 #define NULL_CHECKER(X)
 #define NULL_CHECKER_VAL(X, V)
 #endif
+
+#define DESTROYER(X)             \
+    LOGI(":>Destroying \"%s\"", #X); \
+    free(X);                     \
+    X = NULL
+
+#define NULLER(X)             \
+    LOGI(":>Nulling \"%s\"", #X); \
+    X = NULL
 
 #ifdef __cplusplus
 }
