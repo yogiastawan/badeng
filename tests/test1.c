5a8500c515081e34c0eaa997c0a24997ccf7a86c
Yogi Astawan <yogi.astawan@gmail.com>
2023-09-22 01:10:04 +0800

    fix implementation of engine

--- tests/test1.c
+++ tests/test1.c
@@ -3,7 +3,8 @@
 int main(int argc, char const *argv[])
 {
     BeEngine *eng = be_engine_new();
-    be_engine_run(eng, BE_ENGINE_TYPE_VULKAN);
+    be_engine_init(eng, BE_ENGINE_TYPE_VULKAN);
+    be_engine_run(eng);
     be_engine_destroy(eng);
     return 0;
 }
